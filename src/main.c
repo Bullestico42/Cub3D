4d4d6f4a95da1a172753bbc900deadc5078ada72
Alessio Piscopo <bullestico@macbook-air-de-alessio.home>
2025-07-05 22:02:22 +0200

    test de destruction de fenetre macOS

--- src/main.c
+++ src/main.c
@@ -6,18 +6,35 @@
 /*   By: bullestico <bullestico@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 21:08:36 by bullestico        #+#    #+#             */
-/*   Updated: 2025/07/05 21:32:20 by bullestico       ###   ########.fr       */
+/*   Updated: 2025/07/05 22:01:33 by bullestico       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/cub3d.h"
 
-int	main(int ac, char **av)
+
+#ifdef __linux__
+
+void	destroy_display(void *mlx)
+{
+	mlx_destroy_display(mlx);
+}
+#else
+
+void	destroy_display(void *mlx)
+{
+	(void)mlx;
+}
+
+#endif
+
+int	main(void)
 {
 	void	*mlx = mlx_init();
 	void	*win = mlx_new_window(mlx, 800, 600, "Cub3D");
 
 	mlx_loop(mlx);
+	destroy_display(mlx);
 	free(win);
 
 	return (0);
