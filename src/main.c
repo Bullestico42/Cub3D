117c668678d4551577c1af07e1ed651133f85356
Yokio9 <dmatayi@gmail.com>
2025-07-06 15:03:26 +0200

    ajout d'explication des fonctions

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dimatayi <dimatayi@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 21:08:36 by bullestico        #+#    #+#             */
-/*   Updated: 2025/07/06 14:43:28 by dimatayi         ###   ########.fr       */
+/*   Updated: 2025/07/06 15:02:54 by dimatayi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,12 @@
 
 #ifdef __linux__
 
+/*Fonction responsable de la fermeture du programme.
+Elle prend une string qui sera afficher sur le stderr
+et exit avec le code d'erreur donné en paramètre*/
 int	destroy_display(t_game *game, char *str, int error)
 {
-	ft_putstr_fd(str, 1);
+	ft_putstr_fd(str, 2);
 	if (game->data.mlx && game->data.win)
 		mlx_destroy_window(game->data.mlx, game->data.win);
 	if (game->data.mlx)
@@ -36,11 +39,14 @@ void	destroy_display(void *mlx)
 
 #endif
 
+/* Responsable d'appeler la foncion qui fermera le programme.
+Nécessaire car mlx_hook exige un fonction de ce type : int (*f)(int keycode, void *param);*/
 int	close_game(t_game *game)
 {
 	return(destroy_display(game, "", 0));
 }
-
+/* Le main vérifie que les pointeur mlx et win sont bien initialisés.
+Il permet aussi de fermer la fenêtre avec la croix.*/
 int	main(void)
 {
 	t_game	game;
